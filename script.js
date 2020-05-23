db13c7b968517233509f60a0e44d55edad1a4ccd
Jose Windows <jmportilla@pieriandata.com>
2020-05-23 10:07:00 -0700

    added spotify network project to test

---
+++ script.js
@@ -0,0 +1,53 @@
+var network;
+
+function redrawAll() {
+  // remove positoins
+  for (var i = 0; i < nodes.length; i++) {
+    delete nodes[i].x;
+    delete nodes[i].y;
+  }
+
+  // create a network
+  var container = document.getElementById("mynetwork");
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {
+    nodes: {
+      shape: "dot",
+      scaling: {
+        min: 10,
+        max: 30
+      },
+      font: {
+        size: 12,
+        face: "Tahoma"
+      }
+    },
+    edges: {
+      width: 0.15,
+      color: { inherit: "from" },
+      smooth: {
+        type: "continuous"
+      }
+    },
+    physics: {
+      stabilization: false,
+      barnesHut: {
+        gravitationalConstant: -80000,
+        springConstant: 0.01,
+        springLength: 200
+      }
+    },
+    interaction: {
+      tooltipDelay: 200,
+      hideEdgesOnDrag: true
+    }
+  };
+
+  // Note: data is coming from ./datasources/WorldCup2014.js
+  network = new vis.Network(container, data, options);
+}
+
+redrawAll();
